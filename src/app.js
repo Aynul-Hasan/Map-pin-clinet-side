a054cc76b5cd126a512f6f89de4af5215a9783c7
Aynul-Hasan <aynulhasan003gmail.com>
2021-11-20 12:18:28 +0600

    com

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express= require('express')
 const app = express();
 require('dotenv').config()
 require('./db/database')
-const port= process.env.PORT 
+const port= process.env.PORT ||5000
 const cors= require('cors')
 
 
